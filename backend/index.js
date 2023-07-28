820c18a398c73fc154cf371135143aa830c011f0
dsfs <dsfs>
2023-07-28 14:58:10 +0300

    adasdas

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const epxress = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+
+const app = epxress()
+
+const PORT = process.env.PORT || 5001
+const MONGODB = process.env.MONGODB
+
+app.use(cors())
+app.use(epxress.json())
+
+app.use('/api/user',require('./routes/userRoutes'))
+app.use('/api/posts',require('./routes/postRoutes'))
+
+const start = async()=>{
+    try{
+        await mongoose.connect(MONGODB)
+        .then(()=>console.log('db zbs'))
+        .catch(err=>console.log(err))
+
+        app.listen(PORT,(err)=>{
+            if(err){
+                return console.log('pizdec')
+            }
+            console.log('zbs')
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+start()
